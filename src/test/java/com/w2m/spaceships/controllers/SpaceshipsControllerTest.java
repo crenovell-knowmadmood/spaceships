2771b623b98dce6095b58d25a0830b58040df33b
crenovell-knowmadmood <crenovell@knowmadmood.com>
2024-10-23 17:04:46 +0200

    Correcciones

--- src/test/java/com/w2m/spaceships/controllers/SpaceshipsControllerTest.java
+++ src/test/java/com/w2m/spaceships/controllers/SpaceshipsControllerTest.java
@@ -5,12 +5,14 @@ import static com.w2m.spaceships.constants.MappingConstants.DELETE_URL;
 import static com.w2m.spaceships.constants.MappingConstants.GET_ALL_URL;
 import static com.w2m.spaceships.constants.MappingConstants.GET_BY_ID_URL;
 import static java.util.Objects.nonNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.w2m.spaceships.entities.Spaceship;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -39,18 +42,24 @@ class SpaceshipsControllerTest extends MyTestUtils<Spaceship> {
   @Test
   @Order(1)
   void create() throws Exception {
-    Spaceship spaceship = new Spaceship();
-    spaceship.setId(2);
-    spaceship.setType("tipo");
-    spaceship.setName("Name");
-    final String content = this.writeToJsonString(spaceship);
-    MockHttpServletRequestBuilder requestBuilder = post(CREATE_URL)
+    final int id = 2;
+    Spaceship expected = createSpaceship(id, "type", "Name");
+    final String content = this.writeToJsonString(expected);
+    final MockHttpServletRequestBuilder requestBuilder = post(CREATE_URL)
         .accept(MediaType.APPLICATION_JSON)
         .content(content).contentType(MediaType.APPLICATION_JSON);
-    mockMvc.perform(requestBuilder)
+    final MvcResult result = mockMvc.perform(requestBuilder)
         .andExpect(status().is2xxSuccessful())
+        .andReturn();
+    final MockHttpServletResponse response = result.getResponse();
+    final String contentAsString = response.getContentAsString();
+    final Spaceship actual = this.jsonStringToObject(contentAsString, Spaceship.class);
+    assertNotNull(actual);
+    assertNotNull(actual.getId());
 
-    ;
+    assertEquals(expected.getType(), actual.getType());
+    assertEquals(expected.getName(), actual.getName());
+
   }
 
 
@@ -83,9 +92,18 @@ class SpaceshipsControllerTest extends MyTestUtils<Spaceship> {
   void delete() throws Exception {
     final int id = 2;
     mockMvc.perform(get(DELETE_URL, id))
-        .andExpect(status().isOk())
-        .andExpect(result -> nonNull(result.getResponse()))
+        .andExpect(status().is2xxSuccessful())
     ;
+  }
+
+
+  @NotNull
+  private static Spaceship createSpaceship(final Integer id, final String type, final String name) {
+    Spaceship spaceship = new Spaceship();
+    spaceship.setId(id);
+    spaceship.setType(type);
+    spaceship.setName(name);
+    return spaceship;
   }
 
 
